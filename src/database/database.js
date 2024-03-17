db49bb2d7e4545547b40779bf549512ae5a0c59e
SLIVTIS <leobardo.salinasvl@gmail.com>
2024-03-16 20:23:53 -0600

    Fix: Se agrego el port en la conexion a la base de datos

--- src/database/database.js
+++ src/database/database.js
@@ -4,6 +4,7 @@ import config from "./../config.js";
 const db = new Sequelize(config.db_name, config.db_user, config.db_password, {
     host: config.db_host,
     dialect: "mysql",
+    port: config.db_port,
 });
 
 export default db;
